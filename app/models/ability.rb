fd2eca1a5dd9a85a7b7e4f199ee66eab4c2396e3
Paco Guzman <fjguzman@aspgems.com>
2010-01-18 20:05:46 +0100

    abilities for not logged in users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can [:comment_at_wall, :reply_at_wall], Wall do |wall|
-      wall && (user.profile == wall.profile || user.profile.is_friend_of?(wall.profile))
+    can([:comment_at_wall, :reply_at_wall], Wall) do |wall|
+      wall && user && (user.profile == wall.profile || user.profile.is_friend_of?(wall.profile))
     end
 
-    can [:like_graffity], Graffity do |graffity|
+    can([:like_graffity], Graffity) do |graffity|
       owner = graffity.wall.profile
-      graffity && (user.profile == owner || user.profile.is_friend_of?(owner)) && !graffity.liked_by?(user.profile)
+      graffity && user && (user.profile == owner || user.profile.is_friend_of?(owner)) && !graffity.liked_by?(user.profile)
     end
   end
 end
--- test/unit/ability_test.rb
+++ test/unit/ability_test.rb
@@ -3,6 +3,27 @@ require File.dirname(__FILE__) + '/../test_helper'
 class AbilityTest < ActiveSupport::TestCase
   context "Wall abilities" do
 
+    context "without user not logged in" do
+      setup do
+        @owner = Factory(:member, :login => "ownerious")
+        @current_user = nil
+        @ability = Ability.new(@current_user)
+      end
+
+      should "cannot comment at wall" do
+        assert @ability.cannot?(:comment_at_wall, @owner.profile.wall)
+      end
+
+      should "cannot reply at wall" do
+        assert @ability.cannot?(:reply_at_wall, @owner.profile.wall)
+      end
+
+      should "cannot like a graffity" do
+        @graffity = Factory(:graffity, :wall => @owner.profile.wall, :profile => @owner.profile)
+        assert @ability.cannot?(:like_graffity, @graffity)
+      end
+    end
+
     context "owner" do
       setup do
         @owner = Factory(:member, :login => "ownerious")
