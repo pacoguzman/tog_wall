70e2fdb0e439eac4a0817b767400bd8d8f191440
Alex Eiras <alexeiras@aspgems.com>
2010-01-26 18:27:09 +0100

    Don't override rails

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,7 @@
 class Profile < ActiveRecord::Base
 
-  after_create :create_wall
+  after_create :build_wall
 
   has_one :wall
-
-private
-  def create_wall
-    self.wall = Wall.new
-  end
 
 end
